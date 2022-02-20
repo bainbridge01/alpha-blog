3fe6a1c97dc795ce74f0b02f400b6e094c3cd196
edafe <engr.bainbridge@gmail.com>
2022-02-20 14:53:51 -0800

    saving article and validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,19 @@
 class ArticlesController < ApplicationController
+    def index 
+        @articles = Article.all
+    end
+    
     def show
-        # binding.pry
         @article = Article.find(params[:id])
+    end
+    
+    def new 
+        
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article
     end
 end
