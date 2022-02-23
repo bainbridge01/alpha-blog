caccba05bc6167988608257dd81f5fcb18946ded
edafe <engr.bainbridge@gmail.com>
2022-02-23 14:45:53 -0800

    Implementing DRY coding principle

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index 
         @articles = Article.all
     end
     
     def show
-        @article = Article.find(params[:id])
+
     end
     
     def new 
@@ -12,34 +14,41 @@ class ArticlesController < ApplicationController
     end
     
     def edit 
-        @article = Article.find(params[:id])  
-    end
 
+    end
+    
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
-          flash[:notice] = "Article was created successfully."
-          redirect_to @article 
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article 
         else
-          render 'new'
+            render 'new'
         end
     end
-
+    
     def update 
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
-          flash[:notice] = "Article was updated successfully."
-          redirect_to @article 
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article 
         else
-          render 'edit'
+            render 'edit'
         end
     end
-
+    
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
     end
-
+    
+    private
+    
+    def set_article 
+        @article = Article.find(params[:id])
+    end
+    
+    def article_params 
+        params.require(:article).permit(:title, :description)
+    end
 
 end
